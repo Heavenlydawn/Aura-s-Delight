8ac5994125f62ca3d05db07f22113fec3a6d3458
Heaven Gabriel <gabrielheavenlydawn@gmail.com>
2023-06-29 16:04:54 +0100

    Typing Effect

---
+++ js/script.js
@@ -0,0 +1,46 @@
+let typed = new Typed(".auto-type",
+{
+    strings: ["Savour", "Relish", "Luxuriate"],
+    typeSpeed:150,
+    backSpeed:150,
+    loop:true
+});
+
+// sushi
+let sushi = new Typed(".auto-sushi",
+{
+    strings: ["Savour", "Relish", "Luxuriate"],
+    typeSpeed:150,
+    backSpeed:150,
+    loop:true
+});
+
+// Ice cream
+let ice = new Typed(".auto-ice",
+{
+    strings: ["Savour", "Relish", "Luxuriate"],
+    typeSpeed:150,
+    backSpeed:150,
+    loop:true
+});
+// Cake
+let cake = new Typed(".auto-cake",
+{
+    strings: ["Savour", "Relish", "Luxuriate"],
+    typeSpeed:150,
+    backSpeed:150,
+    loop:true
+});
+
+
+
+// let textIndex = 0;
+// let texts = ["Savour!", "Relish!", "Luxuriate"];
+
+// function changeText() {
+//   let typingText = document.querySelectorAll(".typing-text");
+//   typingText.textContent = texts[textIndex];
+//   textIndex = (textIndex + 1) % texts.length;
+// }
+
+// setInterval(changeText, 2000);
